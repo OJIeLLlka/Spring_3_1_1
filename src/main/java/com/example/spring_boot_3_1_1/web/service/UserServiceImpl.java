5d786cb9649ea803217d84570d8ad5963e984af8
OJIeLLlka <olbastnie@gmail.com>
2022-06-14 15:42:15 +0300

    deleted Transactional over getAllUsers() & getUserById().

--- src/main/java/com/example/spring_boot_3_1_1/web/service/UserServiceImpl.java
+++ src/main/java/com/example/spring_boot_3_1_1/web/service/UserServiceImpl.java
@@ -36,13 +36,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
     @Override
-    @Transactional
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
